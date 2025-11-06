e0fe337508ff1d29aa3a2df83d18d74bd277e0c5
kaushikmak <kaushikmak35@gmail.com>
2025-11-06 22:08:52 +0530

    inital working clock

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/select.h>
 
 // TERMINAL characters
 #define LOW_DENSITY_DOTTED_SQUARE       "░"
@@ -11,16 +13,15 @@
 #define SOLID_CENTER_SQUARE             "■"
 
 // TERMINAL colors
-#define BLACK   "\e[0;30m"
-#define RED     "\e[0;31m"
-#define GREEN   "\e[0;32m"
-#define YELLOW  "\e[0;33m"
-#define BLUE    "\e[0;34m"
-#define PURPLE  "\e[0;35m"
-#define CYAN    "\e[0;35m"
-#define WHITE   "\e[0;37m"
+const struct {
+    const char *BLACK, *RED, *GREEN, *YELLOW, *BLUE, *PURPLE, *CYAN, *WHITE, *RESET;
+} COLOR = {
+    "\e[0;30m", "\e[0;31m", "\e[0;32m", "\e[0;33m",
+    "\e[0;34m", "\e[0;35m", "\e[0;36m", "\e[0;37m",
+    "\x1b[0m"
+};
 
-// digits matrix
+// digits matrix (Unchanged)
 int digits[10][5][5] = {
         // ZERO
         {{1,1,1,1,1}
@@ -36,9 +37,9 @@ int digits[10][5][5] = {
         ,{1,1,1,1,1}},
         // TWO
         {{1,1,1,1,1}
-        ,{1,0,0,0,1}
+        ,{0,0,0,0,1}
         ,{1,1,1,1,1}
-        ,{1,0,0,0,1}
+        ,{1,0,0,0,0}
         ,{1,1,1,1,1}},
         // THREE
         {{1,1,1,1,1}
@@ -77,18 +78,108 @@ int digits[10][5][5] = {
         ,{1,0,0,0,1}
         ,{1,1,1,1,1}},
         // NINE
-        {{0,1,1,1,1}
-        ,{0,1,0,0,1}
-        ,{0,1,1,1,1}
+        {{1,1,1,1,1}
+        ,{1,0,0,0,1}
+        ,{1,1,1,1,1}
         ,{0,0,0,0,1}
         ,{1,1,1,1,1}},
 
 };
 
+// Separator
+int separator[5][3] = {
+    {0, 0, 0},
+    {0, 1, 0},
+    {0, 0, 0},
+    {0, 1, 0},
+    {0, 0, 0}
+};
+#define SEP_WIDTH 3
+#define DIGIT_WIDTH 5
+
+void printNumberRow(int n, int row, const char *colorvalue){
+    int d[2];
+    d[0] = n / 10;
+    d[1] = n % 10;
+
+    for (int k = 0; k < 2; k++) {
+        for (int j = 0; j < DIGIT_WIDTH; j++) {
+            if (digits[d[k]][row][j])
+                printf("%s%s%s", colorvalue, SOLID_SQUARE, COLOR.RESET);
+            else
+                printf(EMPTY);
+        }
+        if (k == 0) {
+             printf(EMPTY);
+        }
+    }
+}
+
+void printSeparatorRow(int row, const char *colorvalue) {
+    for (int j = 0; j < SEP_WIDTH; j++) {
+        if (separator[row][j])
+            printf("%s%s%s", colorvalue, LOW_DENSITY_DOTTED_SQUARE, COLOR.RESET);
+        else
+            printf(EMPTY);
+    }
+}
+
+
 int main(){
-    // get time in seprate entities
-    time_t t = time(NULL);
-    struct tm *currentTime = localtime(&t);
-    printf(PURPLE "%d\n",(currentTime->tm_hour)%12);
+    char inputBuffer[10];
+    // hide cursor
+    printf("\033[?25l");
+
+    while(1){
+        struct timeval tval = {0,0};
+        fd_set fdset;
+        FD_ZERO(&fdset);
+        FD_SET(STDIN_FILENO, &fdset);
+
+        if (select(STDIN_FILENO + 1, &fdset, NULL, NULL, &tval) > 0) {
+            fgets(inputBuffer, sizeof(inputBuffer), stdin);
+            inputBuffer[strcspn(inputBuffer, "\n")] = 0;
+            if (strcmp(inputBuffer, "q") == 0) { 
+                break;
+            }
+        }
+
+        // time
+        time_t t = time(NULL);
+        struct tm *currentTime = localtime(&t);
+
+
+        for (int i = 0; i < 5; i++) {
+
+            printNumberRow((currentTime->tm_hour)%12, i, COLOR.RED);
+            printf("  ");
+
+
+            printSeparatorRow(i, COLOR.YELLOW);
+
+            printNumberRow(currentTime->tm_min, i, COLOR.GREEN);
+            printf("  ");
+
+
+            printSeparatorRow(i, COLOR.YELLOW);
+            printf("  ");
+
+            printNumberRow(currentTime->tm_sec, i, COLOR.BLUE);
+
+            printf("\n");
+        }
+        
+        fflush(stdout);
+
+
+        printf("\033[5A"); 
+        sleep(1);
+    }
+     // show cursor again
+    printf("\033[?25h");
+    // move cursor below clock after exit
+    printf("\033[5B");
+
+
     return 0;
 }
